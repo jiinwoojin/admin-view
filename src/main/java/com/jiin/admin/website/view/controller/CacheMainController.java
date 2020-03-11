d56363a2bf9cecb0fc0b13da4445fe97df81f2f9
tails5555 <tails5555@naver.com>
2020-03-11 18:03:48 +0900

    [kang] Map Proxy 그리드 설정 값 렌더링 기능 구현

--- src/main/java/com/jiin/admin/website/view/controller/CacheMainController.java
+++ src/main/java/com/jiin/admin/website/view/controller/CacheMainController.java
@@ -24,6 +24,7 @@ public class CacheMainController {
 
     @RequestMapping("grids")
     public String cacheGridsPage(Model model){
+        model.addAttribute("boundingBox", cacheService.getBoundingBoxInfoWithCrs());
         return "page/cache/grids";
     }
 
