83113d981ef94d282f18a5ba6856ba3d7d194c1b
tails5555 <tails5555@naver.com>
2020-04-27 18:45:50 +0900

    [kang] UI 디자인 변동 대응 코드 작성 : 구매 라이브러리 Ref. - 구매 Bootstrap 클래스를 참조하여 디자인 변동을 위한 코드 작성. - 각 지도 도시에 대한 대응은 Tab 내용이 안 보이는 현상이 발생. 해결 방안을 찾아야 함.

--- src/main/java/com/jiin/admin/config/SecurityConfig.java
+++ src/main/java/com/jiin/admin/config/SecurityConfig.java
@@ -44,6 +44,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/scss/**",
             "/vendor/**",
             "/vue/**",
+            "/design/**",
             "/favicon.ico"
         );
     }
