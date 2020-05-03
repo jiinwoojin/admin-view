4b3f2736a5ac9a3a31c27aa0f14f6a812d8b8650
박찬신 <parkcs@neutti.com>
2020-05-03 13:47:26 +0900

    ..!

--- src/main/resources/static/js/commons/cfsiStmp2DMap.js
+++ src/main/resources/static/js/commons/cfsiStmp2DMap.js
@@ -39,7 +39,7 @@ var jiMap = function jiMap(options) {
                     var newDrawArray2 = []
                     var exec = false
                     jQuery.each(drawArray2, function(idx, draw){
-                        if(draw.type === 'path' && exec === false){
+                        if(draw.type === 'path' && exec === false) {
                             exec = true
                             var override = Object.assign({}, draw);
                             var background = Object.assign({}, draw);
@@ -55,43 +55,73 @@ var jiMap = function jiMap(options) {
                             var prevx, prevy
                             var startLine = false
                             var percentRate = 1
-                            jQuery.each(pathcoords, function(idx, pathcoord){
+                            jQuery.each(pathcoords, function (idx, pathcoord) {
                                 var xy = pathcoord.split(",")
-                                if(/[Llc]/.test(xy[0])){
+                                if (/[Llc]/.test(xy[0])) {
                                     startLine = true
                                 }
-                                if(/[c]/.test(xy[0])){
+                                if (/[c]/.test(xy[0])) {
                                     percentRate = 0.43
                                 }
-                                var x = parseInt(xy[0].replace(/[LCMVHl]/,""))
+                                var x = parseInt(xy[0].replace(/[LCMVHl]/, ""))
                                 var y = parseInt(xy[1])
-                                if(startLine){
+                                if (startLine) {
                                     x = prevx + x
                                     y = prevy + y
                                 }
-                                console.log(xy, x, y)
-                                if(x < xmin) xmin = x
-                                if(y < ymin) ymin = y
-                                if(x > xmax) xmax = x
-                                if(y > ymax) ymax = y
-                                if(!isNaN(x) && !isNaN(y)){
+                                if (x < xmin) xmin = x
+                                if (y < ymin) ymin = y
+                                if (x > xmax) xmax = x
+                                if (y > ymax) ymax = y
+                                if (!isNaN(x) && !isNaN(y)) {
                                     prevx = x
                                     prevy = y
                                 }
                             })
-                            override.d = "M "+xmin+","+ymin+" H "+xmax+" V "+ymax+" H "+xmin+" V "+ymin+" Z"
+                            override.d = "M " + xmin + "," + ymin + " H " + xmax + " V " + ymax + " H " + xmin + " V " + ymin + " Z"
                             override.fill = "rgb(255,255,255)"
                             override.stroke = "rgb(255,255,255)"
                             override.strokewidth = 0
                             override.fillopacity = 1
                             ymax = Math.round(ymin + (((ymax * percentRate) - ymin) * (1 - percent)))
-                            fillcolor.d = "M "+xmin+","+ymin+" H "+xmax+" V "+ymax+" H "+xmin+" V "+ymin+" Z"
-                            console.log(xmin,ymin,xmax,ymax)
-                            console.log(percent, background.d, fillcolor.d)
+                            fillcolor.d = "M " + xmin + "," + ymin + " H " + xmax + " V " + ymax + " H " + xmin + " V " + ymin + " Z"
                             fillcolor.fill = "rgb(255,255,255)"
                             fillcolor.stroke = "rgb(255,255,255)"
                             fillcolor.strokewidth = 0
                             fillcolor.fillopacity = 1
+                            newDrawArray2.push(override);
+                            newDrawArray2.push(background);
+                            newDrawArray2.push(fillcolor);
+                            newDrawArray2.push(boundry);
+                            console.log(fillcolor)
+                        }else if(draw.type === 'circle' && exec === false){
+                            exec = true
+                            var centerx = draw.cx
+                            var centery = draw.cy
+                            var radius = draw.r
+                            var xmin = centerx - radius
+                            var ymin = centery - radius
+                            var xmax = centerx + radius
+                            var ymax = centery + radius
+                            var background = Object.assign({}, draw);
+                            var boundry = Object.assign({}, draw);
+                            boundry.fill = "rgb(128,224,255,0)"
+                            var override = {}
+                            override.type = "path"
+                            override.fill = "rgb(255,255,255)"
+                            override.fillopacity = 1
+                            override.stroke = "rgb(255,255,255)"
+                            override.strokewidth = 0
+                            override.d = "M " + xmin + "," + ymin + " H " + xmax + " V " + ymax + " H " + xmin + " V " + ymin + " Z"
+                            var fillcolor = {}
+                            fillcolor.type = "path"
+                            fillcolor.fill = "rgb(255,255,255)"
+                            fillcolor.fillopacity = 1
+                            fillcolor.stroke = "rgb(255,255,255)"
+                            fillcolor.strokewidth = 0
+                            ymax = Math.round(ymin + ((ymax - ymin) * (1 - percent)))
+                            fillcolor.d = "M " + xmin + "," + ymin + " H " + xmax + " V " + ymax + " H " + xmin + " V " + ymin + " Z"
+                            console.log(fillcolor, draw)
                             newDrawArray2.push(override);
                             newDrawArray2.push(background);
                             newDrawArray2.push(fillcolor);
