05d49148e180a21554c195d324bba9019bc6877d
tails5555 <tails5555@naver.com>
2020-05-08 15:29:08 +0900

    [kang] 대시보드 측 서버 사양 정보 추출 기능 구현 - 서버 정보로 Java 에서 Linux Shell 로 사양 정보 추출 기능 구현. - Dashboard 측에서 서버 선택 (SI 는 SI 끼리, N-SI 는 N-SI 끼리), 새로 고침 기능 구현. - 디스크, 메모리, CPU 점유율, 서버 접속자 수 등 기능을 구현.

--- src/main/java/com/jiin/admin/website/view/service/DashboardService.java
+++ src/main/java/com/jiin/admin/website/view/service/DashboardService.java
@@ -1,17 +1,20 @@
 package com.jiin.admin.website.view.service;
 
+import com.jcraft.jsch.*;
 import com.jiin.admin.entity.*;
+import com.jiin.admin.website.model.ServerInfoModel;
 import com.jiin.admin.website.server.mapper.CountMapper;
 import com.jiin.admin.website.server.vo.DataCounter;
+import com.jiin.admin.website.server.vo.ServerBasicPerformance;
 import com.jiin.admin.website.view.mapper.AccountMapper;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import javax.persistence.Entity;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class DashboardService {
@@ -21,6 +24,187 @@ public class DashboardService {
     @Resource
     private AccountMapper accountMapper;
 
+    // 서버 설정을 위한 파일 (JSON, properties, yml) 혹은 공간 (DB) 은 협의 이후에 진행 필요.
+    // 여기서 좌측 값을 입력하면, 우측 정보들이 나오는 방향으로 구현할 것.
+    private static final Map<String, ServerInfoModel> serverMap = new HashMap<String, ServerInfoModel>(){{
+        put("B1-Svr1", new ServerInfoModel("B1 SI Server 1", "SI", "192.168.1.141", "jiapp", "jiin0701!"));
+        put("B1-Svr2", new ServerInfoModel("B1 SI Server 2", "SI", "192.168.1.142", "jiapp", "jiin0701!"));
+
+        put("NB1-Svr1", new ServerInfoModel("B1 N-SI Server 1", "N-SI", "192.168.1.152", "jiapp", "jiin0701!"));
+        put("NB1-Svr2", new ServerInfoModel("B1 N-SI Server 2", "N-SI", "192.168.1.153", "jiapp", "jiin0701!"));
+
+        put("U3-Svr1", new ServerInfoModel("U3 SI Server 1", "SI", "192.168.1.155", "jiapp", "jiin0701!"));
+        put("U3-Svr2", new ServerInfoModel("U3 SI Server 2", "SI", "192.168.1.156", "jiapp", "jiin0701!"));
+
+        put("NU3-Svr1", new ServerInfoModel("U3 N-SI Server 1", "N-SI", "192.168.1.158", "jiapp", "jiin0701!"));
+        put("NU3-Svr2", new ServerInfoModel("U3 N-SI Server 2", "N-SI", "192.168.1.159", "jiapp", "jiin0701!"));
+
+        put("GOC-Svr1", new ServerInfoModel("GOC SI Server 1", "SI", "192.168.1.161", "jiapp", "jiin0701!"));
+        put("GOC-Svr2", new ServerInfoModel("GOC SI Server 2", "SI", "192.168.1.162", "jiapp", "jiin0701!"));
+
+        put("B1-CDS", new ServerInfoModel("B1 CDS", "CDS", "192.168.1.164", "jiapp", "jiin0701!"));
+        put("U3-CDS", new ServerInfoModel("U3 CDS", "CDS", "192.168.1.165", "jiapp", "jiin0701!"));
+    }};
+
+    // SI 일 때 보여줄 정보들 (B1, U3, GOC) + CDS
+    private static final Map<String, List<String>> SI_CONNECT = new HashMap<String, List<String>>(){{
+        put("B1", new ArrayList<>(Arrays.asList("B1-Svr1:B1 SI Server 1", "B1-Svr2:B1 SI Server 2")));
+        put("U3", new ArrayList<>(Arrays.asList("U3-Svr1:U3 SI Server 1", "U3-Svr2:U3 SI Server 2")));
+        put("GOC", new ArrayList<>(Arrays.asList("GOC-Svr1:GOC SI Server 1", "GOC-Svr2:GOC SI Server 2")));
+    }};
+
+    // N-SI 일 때 보여줄 정보들 (B1, U3) + CDS
+    private static final Map<String, List<String>> N_SI_CONNECT = new HashMap<String, List<String>>(){{
+        put("B1", new ArrayList<>(Arrays.asList("NB1-Svr1:B1 N-SI Server 1", "NB1-Svr2:B1 N-SI Server 2")));
+        put("U3", new ArrayList<>(Arrays.asList("NU3-Svr1:U3 N-SI Server 1", "NU3-Svr2:U3 N-SI Server 2")));
+    }};
+
+    private static final int SFTP_PORT = 22;
+    private static final int BUFFER_SIZE = 1024;
+
+    // Java 기반 Linux Shell 작동
+    private String getShellCMD(ServerInfoModel serverInfo, String command) {
+        Session session = null;
+        Channel channel = null;
+        InputStream is;
+        StringBuffer sb = new StringBuffer();
+        try {
+            JSch jsch = new JSch();
+            session = jsch.getSession(serverInfo.getUsername(), serverInfo.getIpAddress(), SFTP_PORT);
+            session.setPassword(serverInfo.getPassword());
+
+            Properties config = new Properties();
+            config.put("StrictHostKeyChecking", "no");
+            session.setConfig(config);
+
+            session.connect();
+
+            channel = session.openChannel("exec");
+
+            ChannelExec exec = (ChannelExec) channel;
+            exec.setCommand(command);
+            exec.setInputStream(null);
+
+            is = channel.getInputStream(); // 출력 스트림
+            byte[] buffer = new byte[BUFFER_SIZE];
+
+            exec.connect();
+            while(true){
+                while (is.available() > 0) {
+                    int i = is.read(buffer, 0, BUFFER_SIZE);
+                    if (i < 0) break;
+                    sb.append(new String(buffer, 0, i));
+                }
+
+                if (channel.isClosed()) {
+                    if (is.available() > 0) continue;
+                    break;
+                }
+
+                TimeUnit.MILLISECONDS.sleep(100);
+            }
+        } catch (JSchException | IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (channel != null) {
+                channel.disconnect();
+            }
+            if (session != null) {
+                session.disconnect();
+            }
+            return sb.toString();
+        }
+    }
+
+    public ServerBasicPerformance getServerBasicPerformance(String key){
+        ServerBasicPerformance sp = new ServerBasicPerformance();
+        int errCnt = 0;
+
+        // 서버 이름 설정
+        sp.setServerName(serverMap.get(key).getName());
+
+        // RAM 정보
+        // 2번 : 전체, 3번 : 사용, 7번 : 사용 가능 (단위 : MB)
+        String cpuRes = getShellCMD(serverMap.get(key), "free -m | awk \'NR == 2 {print $2,$3,$7}\'");
+        if(cpuRes != null){
+            String[] split = cpuRes.split(" ");
+            if(split.length == 3) {
+                sp.setTotalMemory(Long.parseLong(split[0].trim()));
+                sp.setUsedMemory(Long.parseLong(split[1].trim()));
+                sp.setAvailableMemory(Long.parseLong(split[2].trim()));
+            } else {
+                errCnt += 1;
+            }
+        }
+
+        // Disk 정보
+        // 2번 : 전체, 3번 : 사용, 4번 : 나머지 (단위 : GB)
+        String diskRes = getShellCMD(serverMap.get(key), "df -P | grep -v ^Filesystem | awk \'{sum_tot += $2; sum_used += $3; sum_rem += $4} END { print sum_tot/1024/1024, sum_used/1024/1024, sum_rem/1024/1024 }\'");
+        if(diskRes != null){
+            String[] split = diskRes.split(" ");
+            if(split.length > 0 ) {
+                sp.setTotalCapacity(Double.parseDouble(split[0].trim()));
+                sp.setUsedCapacity(Double.parseDouble(split[1].trim()));
+            } else {
+                errCnt += 1;
+            }
+        }
+
+        // CPU 정보
+        String cpuUsage = getShellCMD(serverMap.get(key), "mpstat | tail -1 | awk \'{print 100-$NF}\'");
+        if(cpuUsage != null){
+            sp.setCpuUsage(Double.parseDouble(cpuUsage.trim()));
+        } else {
+            errCnt += 1;
+        }
+
+        // 사용자 접속 정보
+        String connections = getShellCMD(serverMap.get(key), "who | awk \'END{print NR}\'");
+        if(connections != null){
+            sp.setConnections(Long.parseLong(connections.trim()));
+        } else {
+            errCnt += 1;
+        }
+
+        if(errCnt == 4){
+            sp.setStatus("OFF");
+        } else if(errCnt != 0){
+            sp.setStatus("ERROR");
+        } else {
+            sp.setStatus("ON");
+        }
+
+        return sp;
+    }
+
+    // 서버 기본 정보를 가져오는 함수
+    public Map<String, ServerBasicPerformance> getFirstServerBasicPerformance(String type){
+        Map<String, ServerBasicPerformance> map = new LinkedHashMap<>();
+        if(type.equals("SI")){
+            map.put("B1", getServerBasicPerformance("B1-Svr1"));
+            map.put("U3", getServerBasicPerformance("U3-Svr1"));
+            map.put("GOC", getServerBasicPerformance("GOC-Svr1"));
+        } else {
+            map.put("B1", getServerBasicPerformance("NB1-Svr1"));
+            map.put("U3", getServerBasicPerformance("NU3-Svr1"));
+        }
+        return map;
+    }
+
+    // 연결 관계를 가져오는 함수
+    public Map<String, List<String>> getConnectRelations(String type){
+        Map<String, List<String>> map = new HashMap<>();
+        Map<String, List<String>> based = type.equals("SI") ? SI_CONNECT : N_SI_CONNECT;
+        for(String key : based.keySet()) {
+            List<String> list = new ArrayList<>(based.get(key));
+            if (key.equals("B1")) list.add("B1-CDS:B1 CDS");
+            if (key.equals("U3")) list.add("U3-CDS:U3 CDS");
+            map.put(key, list);
+        }
+        return map;
+    }
+
+    // 데이터 개수를 가져오는 함수
     public DataCounter getDataCounter(){
         DataCounter dataCounter = new DataCounter();
         dataCounter.setMapCount(countMapper.countByTableName(MapEntity.class.getAnnotation(Entity.class).name()));
--- src/main/resources/static/js/page/dashboard.js
+++ src/main/resources/static/js/page/dashboard.js
@@ -2,52 +2,61 @@ $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 });
 
-function onmouseenter_server_status(center){
-    // AJAX 서버 요청 : 서버 상태 호출
+function event_refresh_server(dom){
+    var value = dom.value;
+    var id = '';
 
-    $("#" + event.target.id)
-        .attr('data-original-title', `
-            <p id="server_basic" class="text-left" style="color: white; overflow-x: hidden;">
-                전체 용량 : <span>1.00</span> / <span>9876.54</span> GB <br/>
-                메모리 용량 : <span>4.11</span> / <span>80.32</span> GB <br/>
-                CPU 점유율 : <span>76</span> % <br/>
-                서버 커넥션 Pool : <span>5</span>
-            </p>
-            <p id="server_services" class="text-left" style="color: white; overflow-x: hidden;">
-                MapServer 상태 : <i class="fas fa-check"></i> <br/> 
-                MapProxy 상태 : <i class="fas fa-check"></i> <br/>
-                Terrain Builder 상태 : <i class="fas fa-check"></i> <br/>
-                Mapnik 상태 : <i class="fas fa-check"></i> <br/>
-                Tegola 상태 : <i class="fas fa-check"></i> <br/>
-                PostgreSQL 상태 : <i class="fas fa-check"></i> <br/>
-                Syncthing 상태 : <i class="fas fa-times"></i> <br/>
-                Docker 상태 : <i class="fas fa-times"></i>
-            </p>
-        `)
-        .tooltip('show');
-}
+    if(dom.id.startsWith('B1')){
+        id = 'B1';
+    } else if(dom.id.startsWith('U3')){
+        id = 'U3';
+    } else {
+        id = 'GOC';
+    }
 
-function onmouseenter_sync_status(center){
-    // AJAX 서버 요청 : 동기화 상태 호출
+    if(!value) {
+        value = document.getElementById(id + "_change_server").value;
+    }
 
-    $("#" + event.target.id)
-        .attr('data-original-title', `
-            <p id="sync_basic" class="text-left" style="color: white; overflow-x: hidden;">
-                최근 진행 일자<br/>
-                - <span>2020-04-22 10:00:00</span><br/>
-                최근 동기화 파일<br/>
-                - <span>A.tif 이외 10 개</span><br/>
-                동기화 기본 디렉토리<br/>
-                - <span>/data/center/sync</span>
-            </p>
-            <p id="sync_services" class="text-left" style="color: white; overflow-x: hidden;">
-                PGPool 2 상태 : <i class="fas fa-check"></i> <br/> 
-                Syncthing 상태 : <i class="fas fa-check"></i> <br/>
-                타 체계 파일 동기화 준비 : <i class="fas fa-check"></i> <br/>
-                타 체계 DB 동기화 준비 : <i class="fas fa-check"></i> <br/>
-                타 센터 파일 동기화 준비 : <i class="fas fa-check"></i> <br/>
-                타 센터 DB 동기화 준비 : <i class="fas fa-check"></i> <br/>
-            </p>
-        `)
-        .tooltip('show');
+    $.ajax({
+        url: CONTEXT + '/server/api/dashboard/performance/' + value,
+        type: 'GET',
+        contentType : 'application/json',
+        success: function(res){
+            if(res != null){
+                for(var key in res){
+                    if(!key.endsWith('status')) {
+                        document.getElementById(id + '_' + key).innerText = res[key];
+                    } else {
+                        switch(res[key]){
+                            case "ON" :
+                                document.getElementById(id + '_serverTitle').className = 'text-sm font-weight-bold text-uppercase mb-1 text-success';
+                                document.getElementById(id + '_serverStatus').className = 'text-sm font-weight-bold text-uppercase mb-1 text-success';
+                                document.getElementById(id + '_serverStatus').innerHTML = '<i class="h1 fas fa-check-circle"></i>';
+                                break;
+                            case "OFF" :
+                                document.getElementById(id + '_serverTitle').className = 'text-sm font-weight-bold text-uppercase mb-1 text-danger';
+                                document.getElementById(id + '_serverStatus').className = 'text-sm font-weight-bold text-uppercase mb-1 text-danger';
+                                document.getElementById(id + '_serverStatus').innerHTML = '<i class="h1 fas fa-check-times"></i>';
+                                break;
+                            case "ERROR" :
+                                document.getElementById(id + '_serverTitle').className = 'text-sm font-weight-bold text-uppercase mb-1 text-danger';
+                                document.getElementById(id + '_serverStatus').className = 'text-sm font-weight-bold text-uppercase mb-1 text-danger';
+                                document.getElementById(id + '_serverStatus').innerHTML = '<i class="h1 fas fa-exclamation-triangle"></i>';
+                                break;
+                        }
+                    }
+                }
+            }
+        },
+        error: function(e){
+            console.log(e);
+        },
+        beforeSend: function(){
+            jiCommon.showLoading();
+        },
+        complete: function(){
+            jiCommon.hideLoading();
+        }
+    });
 }
