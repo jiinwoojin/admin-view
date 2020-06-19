8558da51febdc45b1c6f53b89c914eb88099b2d4
tails5555 <tails5555@naver.com>
2020-06-19 11:09:36 +0900

    [kang] 1 -> 0.1 단위 수정

--- src/main/java/com/jiin/admin/website/view/component/MapVersionManagement.java
+++ src/main/java/com/jiin/admin/website/view/component/MapVersionManagement.java
@@ -169,7 +169,7 @@ public class MapVersionManagement {
         if(pureNewLayer.size() > 0) {
             nextMaxVersion = Collections.max(pureNewLayer.stream().map(o -> o.getVersion()).collect(Collectors.toSet()));
         }
-        return Math.max(Double.parseDouble(String.format("%.1f", newVersion + 1)), nextMaxVersion);
+        return Math.max(Double.parseDouble(String.format("%.1f", newVersion + 0.1)), nextMaxVersion);
     }
 
     /**
